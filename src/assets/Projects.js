14eeb70b563aa78370c13f620d8618eb12b31ae1
ErnestTramp <ernesttramp@gmail.com>
2023-08-28 12:13:46 +0100

    Reworked Carousel

--- src/assets/Projects.js
+++ src/assets/Projects.js
@@ -1,1 +1,45 @@
-const Projects = [];
+const Projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    type: "Web Application",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur tortor non ante vehicula, et tincidunt libero dignissim.",
+    imageUrl: "project1.jpg",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    type: "Mobile App",
+    description:
+      "Vestibulum eget libero eget odio eleifend suscipit. Sed posuere vehicula nunc, eu facilisis odio ultricies nec.",
+    imageUrl: "project2.jpg",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    type: "E-commerce Website",
+    description:
+      "Fusce dignissim odio eu vestibulum. Sed sed metus eget velit tincidunt feugiat in at tortor.",
+    imageUrl: "project3.jpg",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    type: "Data Analysis",
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    imageUrl: "project4.jpg",
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    type: "Game Development",
+    description:
+      "Duis eu quam ut neque facilisis convallis. Nulla non neque vel purus varius ultrices.",
+    imageUrl: "project5.jpg",
+  },
+  // Add more projects as needed
+];
+
+export default Projects;
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,90 +1,47 @@
 import { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 import './ProjectPage.css';
 import StarsCanvas from './StarsCanvas';
-import throttle from 'lodash/throttle';
-
-function CustomCarousel(props) {
-    const slider = useRef(null);
-    let isDown = useRef(false);
-    let startX = useRef(null);
-    let scrollLeft = useRef(null);
-  
-    useEffect(() => {
-      if (slider && slider.current) {
-        let sliderRef = slider.current;
-        sliderRef.addEventListener("mousedown", one);
-        sliderRef.addEventListener("mousedown", two);
-        sliderRef.addEventListener("mouseleave", three);
-        sliderRef.addEventListener("mouseup", four);
-        sliderRef.addEventListener("mousemove", five);
-  
-        return () => {
-          sliderRef.removeEventListener("mousedown", one);
-          sliderRef.removeEventListener("mousedown", two);
-          sliderRef.removeEventListener("mouseleave", three);
-          sliderRef.removeEventListener("mouseup", four);
-          sliderRef.removeEventListener("mousemove", five);
-        };
-      }
-    }, []);
-  
-    function one(e) {
-      isDown.current = true;
-      startX.current = e.pageX - slider.current.offsetLeft;
-      scrollLeft.current = slider.current.scrollLeft;
-    }
-  
-    function two(e) {
-      isDown.current = true;
-      startX.current = e.pageX - slider.current.offsetLeft;
-      scrollLeft.current = slider.current.scrollLeft;
-    }
-  
-    function three() {
-      isDown.current = false;
-    }
-  
-    function four() {
-      isDown.current = false;
-    }
-  
-    function five(e) {
-      if (!isDown.current) return;
-      e.preventDefault();
-      const x = e.pageX - slider.current.offsetLeft;
-      const walk = x - startX.current;
-      slider.current.scrollLeft = scrollLeft.current - walk;
-    }
-  
-    return (
-      <div className="carousel" ref={slider}>
-        {props.children}
-      </div>
-    );
-}
-
-function Box({ index }) {
-    return <div className="carousel-item">Box {index}</div>;
-  }
-  
+import Projects from '../assets/Projects';
 
 export default function ProjectPage() {
+    const [width, setWidth] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(0);
+    const carousel = useRef();
+
+    useEffect(() => {
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
+
+    const handleClick = (id) => {
+        setSelectedItem(id);
+        console.log(selectedItem);
+    }
 
     return (
         <div className="centerDiv-2">
             <StarsCanvas />
             <div className="text-desc">
                 <p>MY PROJECTS</p>
-            </div>                
-                <CustomCarousel>
-                    {new Array(10).fill("").map((_, index) => {
-                    return <Box index={index} key={index} />;
-                    })}
-                </CustomCarousel>
+            </div>
+            <motion.div
+                ref={carousel}
+                className='carousel'
+                drag="x"
+                dragConstraints={{ right: 100, left: -width }}
+            >
+                {Projects.map((project, index) => {
+                    return (
+                        <motion.div className="carousel-item" key={project.id} onClick={() => {handleClick(project.id)}}>
+                            <h1>{project.title}</h1>
+                        </motion.div>
+                    )
+                })}
+            </motion.div>
             <div className="project-text">
-                <p className='project-type'>selectedProject.type</p>
-                <h2 className='project-title'>selectedProject.title</h2>
-                <p>selectedProject.description</p>
+                <p className='project-type'>1</p>
+                <h2 className='project-title'>2</h2>
+                <p>3</p>
             </div>
         </div>
     );
