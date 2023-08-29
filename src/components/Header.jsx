05f25fcdcdab944b0783e698e88960d86e4f4505
ErnestTramp <ernesttramp@gmail.com>
2023-08-29 21:54:15 +0100

    Headroom Functionallity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import './Header.css'
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import Headroom from 'react-headroom';
 
 export default function Header() {
     return (
+          <Headroom style={{transition: 'all .5s ease-in-out'}}>
         <header>
           <div className="nav">
             <Link to="projects" spy={true} offset={-30} smooth={true} duration={800} className="elem projects">
@@ -22,6 +24,8 @@ export default function Header() {
             <FontAwesomeIcon onClick={() => {window.open("https://tiktok.com/@ernest.tramp")}} icon={faTiktok} className='tiktok icon' size='2xl' />
             <FontAwesomeIcon onClick={() => {window.open("https://youtube.com/@RadialFlipz")}} icon={faYoutube} className='youtube icon' size='2xl' />
           </div>
+         
         </header>
+          </Headroom>
       );
 }
